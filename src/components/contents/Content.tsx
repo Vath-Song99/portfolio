3c24124c6615e9da17958803a4d9e3ec5d8a1417
Smoeury-Songvat <s.songvat@dgc.gov.kh>
2025-06-16 16:32:11 +0700

    refactor: clean up Experience component imports

--- src/components/contents/Content.tsx
+++ src/components/contents/Content.tsx
@@ -21,9 +21,8 @@ const Content = ({ theme }: ContentProps) => {
         }}
       >
         <Description theme={theme} />
-        <Experience theme={theme}/>
         <Education theme={theme} />
-        <Experience theme={theme} />
+        <Experience theme={theme}/>
       </motion.div>
     </main>
   );
